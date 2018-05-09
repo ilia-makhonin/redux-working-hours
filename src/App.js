6cdf25417f1ac8dfd255f180e1de5aa8c33ce765
Ilia Makhonin <noizemkutane77@gmail.com>
2018-05-09 17:11:39 +0300

    An application was created using ReactJS and Redux. Materials - CodeDojo

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import './App.css';
+
+function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case 'INCREMENT': return { count: state.count + action.amount };
+        case 'DECREMENT': return { count: state.count - action.amount };
+        case 'RESET': return { count: 0 };
+        default: return state;
+    }
+}
+
+const initialState = { count: 0} ;
+
+function increment(amount) {
+    return { type: 'INCREMENT', amount };
+}
+
+function decrement(amount) {
+    return { type: 'DECREMENT', amount };
+}
+
+function reset() {
+    return { type: 'RESET' };
+}
+
+const store = createStore(reducer, initialState);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
+
+  increment() {
+      let amount = parseInt(this.refs.amount.value || 1);
+      store.dispatch(increment(amount));
+  }
+
+  decrement() {
+      let amount = parseInt(this.refs.amount.value || 1);
+      store.dispatch(decrement(amount));
+  }
+
+  reset() {
+    store.dispatch(reset());
+  }
+
+  render() {
+    const count = store.getState().count;
+    return (
+      <div className="counter">
+        <span className="count">{count}</span>
+
+        <div className="buttons">
+          <button className="decrement" onClick={this.decrement}>-</button>
+          <button className="reset" onClick={this.reset}>R</button>
+          <button className="increment" onClick={this.increment}>+</button>
+        </div>
+
+          <input type="text" ref="amount" defaultValue={1} />
+      </div>
+    );
+  }
+}
+
+export default App;
